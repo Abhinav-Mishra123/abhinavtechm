a4f6dcc285e22df31643be02f901945df312a896
Abhinav-Mishra123 <contactatabhinavmishra@gmail.com>
2022-07-19 12:51:23 +0530

    add reveal in every section and design owl  carousal navigation  and also add reveal js

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ window.onload =()=>{
     
    }
 }
+
+
+// Scrolling animation 
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("activeme");
+        } else {
+            reveals[i].classList.remove("activeme");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
